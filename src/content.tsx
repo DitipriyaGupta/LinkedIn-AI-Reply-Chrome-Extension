6533f9c1d09b349baa8b17b157960f4458dcd707
Ditipriya Gupta <mail.ditipriyagupta@gmail.com>
2024-07-11 02:22:10 +0530

    Feat : Added functionality to close the modal by clicking anywhere outside

--- src/content.tsx
+++ src/content.tsx
@@ -3,9 +3,9 @@ import type { PlasmoCSConfig } from "plasmo"
 import { useEffect, useState } from "react"
 
 import { CountButton } from "~features/CountButton"
+import Modal from "~features/Modal"
 
 import AiIcon from "../assets/AI.svg"
-import Modal from "~features/Modal"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://*.linkedin.com/*"]
@@ -19,63 +19,51 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   const [isFocus, setIsFocus] = useState(false)
-  const[isOpen,setIsOpen]=useState(false)
-  const handleFocus=()=>{
+  const [isOpen, setIsOpen] = useState(false)
+  const handleFocus = () => {
     const textarea = document.querySelector(
       ".msg-form__contenteditable"
     ) as HTMLTextAreaElement
-    // if (textarea) {
-    //   textarea.addEventListener("focus", (e) => {
-        setIsFocus(true)
-        const container = document.createElement("div")
-        container.className = "ai-icon"
-        container.setAttribute(
-          "style",
-          "position:absolute; bottom:0; right:0rem;"
-        )
-        const imgElement = document.createElement("img")
-        imgElement.src = AiIcon
-        imgElement.alt = "ai-icon"
-        imgElement.setAttribute(
-          "style",
-          "width: 32px; height: 32px; cursor:pointer;"
-        )
-        container.addEventListener("click",()=>{
-          setIsOpen(true)
-        })
-        container.appendChild(imgElement)
-        textarea?.appendChild(container)
-       
-      
-    }
-  
-  const handleBlur=()=>{
-    const textarea = document.querySelector(".msg-form__contenteditable");
-    const container = textarea?.querySelector(".ai-icon"); 
-    container?.remove();
+    setIsFocus(true)
+    const container = document.createElement("div")
+    container.className = "ai-icon"
+    container.setAttribute("style", "position:absolute; bottom:0; right:0rem;")
+    const imgElement = document.createElement("img")
+    imgElement.src = AiIcon
+    imgElement.alt = "ai-icon"
+    imgElement.setAttribute(
+      "style",
+      "width: 32px; height: 32px; cursor:pointer;"
+    )
+    container.addEventListener("click", () => {
+      setIsOpen(true)
+    })
+    container.appendChild(imgElement)
+    textarea?.appendChild(container)
+  }
+
+  const handleBlur = () => {
+    const textarea = document.querySelector(".msg-form__contenteditable")
+    const container = textarea?.querySelector(".ai-icon")
+    container?.remove()
   }
   useEffect(() => {
-    // const observer = new MutationObserver(() => {
-      
-    //   observer.disconnect()
-    // })
-
-    // observer.observe(document.body, { childList: true, subtree: true })
-    // return () => observer.disconnect()
-    const intervalId=setInterval(()=>{
-      const textarea = document.querySelector(".msg-form__contenteditable");
-      if(textarea){
-        textarea.addEventListener("focus",handleFocus)
-        textarea.addEventListener("blur",handleBlur)
-clearInterval(intervalId)
-        
+    const intervalId = setInterval(() => {
+      const textarea = document.querySelector(".msg-form__contenteditable")
+      if (textarea) {
+        textarea.addEventListener("focus", handleFocus)
+        textarea.addEventListener("blur", handleBlur)
+        clearInterval(intervalId)
       }
-
-    },4000)
-    return ()=>clearInterval(intervalId)
+    }, 3000)
+    return () => clearInterval(intervalId)
   }, [])
 
-  return isFocus ? <div className="w-full"><Modal isOpen={isOpen} setIsOpen={setIsOpen} /></div> : null
+  return isFocus ? (
+    <div className="w-full">
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+    </div>
+  ) : null
 }
 
 export default PlasmoOverlay
--- src/features/Modal.tsx
+++ src/features/Modal.tsx
@@ -1,97 +1,115 @@
-
-import { useState } from "react";
 import GenerateImage from "assets/Generate.svg"
+import Insert from "assets/Insert.svg"
+import RegenerateImage from "assets/Regenerate.svg"
+import { useState } from "react"
+
 interface ModalProps {
-  isOpen: boolean,
+  isOpen: boolean
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
-const Modal : React.FC<ModalProps>  = ({isOpen,setIsOpen}) => {
-  const SAMPLE_RESPONSE="Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask."
-  const [isGenerated, setIsGenerated] = useState(false);
-  const [input, setInput] = useState("");
-  const [query, setQuery] = useState("");
+const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen }) => {
+  const SAMPLE_RESPONSE =
+    "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask."
+  const [isGenerated, setIsGenerated] = useState(false)
+  const [input, setInput] = useState("")
+  const [query, setQuery] = useState("")
 
-  const handleQuery = (e : any) => {
-    e.preventDefault();
-    setQuery(input);
-    setInput("");
-  };
+  const handleQuery = (e: any) => {
+    e.preventDefault()
+    setQuery(input)
+    setInput("")
+  }
 
- const closeModal=()=>{
-  setIsOpen(false)
-
- }
- const handleInsert=()=>{
-  closeModal()
-  const textarea = document.querySelector(".msg-form__contenteditable");
-  textarea.children[0].ariaLabel=""
-  console.log(textarea)
-  console.log(textarea.children[0])
-  textarea.children[0].innerHTML=SAMPLE_RESPONSE
-  
-  setInput("")
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+  const handleInsert = () => {
+    closeModal()
+    const placeholder=document.querySelector(".msg-form__placeholder")
+    const textarea = document.querySelector(".msg-form__contenteditable")
+    placeholder?.remove()
+    textarea.children[0].innerHTML = SAMPLE_RESPONSE
+    setInput("")
+    setQuery("")
     setIsGenerated(false)
-  
- }
- const handleIsOpenCheck=()=>{
-  isOpen?closeModal():null
- }
+  }
+  const handleIsOpenCheck = () => {
+     closeModal() 
+  }
+  const handleModal=(e:React.MouseEvent)=>{
+e.stopPropagation()
+  }
   return (
     <>
-  {isOpen && ( <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-      <div className="p-3 bg-white w-[350px] rounded-md">
-      {query && (
-        <>
-           <div className="flex justify-end">
-           <div className="p-2 bg-gray-200 w-[300px] flex justify-end items-start rounded-md mb-3">
-             <p className="text-[#666D80]">{query}</p>
-           </div>
-
-           </div>
-           <div className="flex justify-start">
-           <div className="p-2 bg-blue-100 w-[400px] flex justify-start items-start rounded-md mb-3">
-             <p className="text-[#666D80]">{SAMPLE_RESPONSE}</p>
-           </div>
-
-           </div></>
-        ) }
-       
-          <input
-            placeholder="Your prompt"
-            type="text"
-            className="rounded-md p-2 border-2 border-gray-300 w-full mb-3"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-
-          <div className="flex justify-end">
-            {isGenerated ? (
-              <div className="flex gap-2">
-                <button className="h-8 w-24  rounded text-gray-500 text-semibold border-2 border-gray-400" onClick={()=>handleInsert()}>
-                  Insert
-                </button>
-                <button className="h-8 w-24 bg-blue-500 rounded text-white">
-                  Regenerate
-                </button>
-              </div>
-            ) : (
-              <button
-                onClick={(e) => {setIsGenerated(!isGenerated) 
-                    handleQuery(e)
-                 }}
-
-                className=" h-8 w-24 bg-blue-500 rounded text-white flex tracking-wide"
-              >
-                <span><img src={GenerateImage} alt="image" className="h-5 w-5"/></span>Generate
-              </button>
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center" onClick={handleIsOpenCheck}>
+          <div className="p-3 bg-[#F9FAFB] w-[350px] rounded-xl" onClick={handleModal}>
+            {query && (
+              <>
+                <div className="flex justify-end">
+                  <div className="p-2 bg-gray-200 max-w-[200px] flex justify-end items-start rounded-md mb-3">
+                    <p className="text-[#666D80] tracking-wider">{query}</p>
+                  </div>
+                </div>
+                <div className="flex justify-start">
+                  <div className="p-2 bg-blue-100 max-w-[300px] flex justify-start items-start rounded-md mb-3">
+                    <p className="text-[#666D80] tracking-wider">
+                      {SAMPLE_RESPONSE}
+                    </p>
+                  </div>
+                </div>
+              </>
             )}
-          </div>
-        
-       
-      </div>
-    </div>)}
-    </>
-  );
-};
+<div className="p-2">
+            <input
+              placeholder="Your prompt"
+              type="text"
+              className="rounded-md  border-2 border-gray-300 w-full mb-3"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            /></div>
 
-export default Modal;
+            <div className="flex justify-end">
+              {isGenerated ? (
+                <div className="flex gap-2">
+                  <button
+                    className="h-10 w-24  rounded text-gray-500 text-semibold border-2 border-gray-400"
+                    onClick={() => handleInsert()}>
+                    <span className="flex justify-center items-center gap-2">
+                      <img src={Insert} alt="image" className="h-3 w-3" />
+                      <p className="tracking-wider font-semibold">Insert</p>
+                    </span>
+                  </button>
+                  <button className="h-10 w-36 bg-blue-500 rounded text-white">
+                    <span className="flex justify-center items-center gap-2">
+                      <img
+                        src={RegenerateImage}
+                        alt="image"
+                        className="h-5 w-5"
+                      />
+                      <p className="tracking-wider font-semibold">Regenerate</p>
+                    </span>
+                  </button>
+                </div>
+              ) : (
+                <button
+                  onClick={(e) => {
+                    setIsGenerated(!isGenerated)
+                    handleQuery(e)
+                  }}
+                  className="h-10 w-32 bg-blue-500 rounded text-white ">
+                  <span className="flex justify-center items-center gap-2">
+                    <img src={GenerateImage} alt="image" className="h-5 w-5" />
+                    <p className="tracking-wider font-semibold">Generate</p>
+                  </span>
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Modal
